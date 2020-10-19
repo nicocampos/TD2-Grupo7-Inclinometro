99f29e5ae7d70251f41d07addf2b9cd188657a98
nicocampos <nicocampos_97@hotmail.com>
2020-10-19 16:18:08 -0300

    Actualizo proyecto. Funcionando con Threshold.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -39,9 +39,9 @@
 #define dwt_reset() 		{DWT->CYCCNT=0;}
 #define dwt_read() 			(DWT->CYCCNT)
 
-#define MODE_KEY			GPIOB, inputMode
-#define HOLD_KEY			GPIOB, inputHold
-#define ZERO_KEY			GPIOB, inputZero
+#define MODE_KEY			GPIOB, inputMode_Pin
+#define HOLD_KEY			GPIOB, inputHold_Pin
+#define ZERO_KEY			GPIOB, inputZero_Pin
 
 #define MODE				1
 #define ZERO				2
@@ -54,8 +54,10 @@
 #define MAX_LEN_TASK_LIST	(8)
 #define TICK_SISTEMA		(10)
 #define TIMEOUT_I2C			10
-#define PROMEDIO			5
+#define PROMEDIO			50
 #define POS_GRADOS			10,30	//30,30
+#define POS_GRADOSX			10,15
+#define POS_GRADOSY			10,30
 
 // ESTADOS - MODOS
 #define MODO_MEDIR			0
@@ -64,6 +66,8 @@
 
 #define ON					1
 #define OFF					0
+
+#define THRESHOLD			0.13
 
 //#define POS_SIGNO			5,35
 //#define __SET_IWDG
@@ -108,9 +112,15 @@ uint8_t antirebote (uint8_t lectura_actual);
 /* USER CODE BEGIN 0 */
 MPU6050_t MPU6050;
 
+float offset_x = 0;
+
+float offset_y = 0;
+
 uint8_t tecla = NO_KEY;
 
-uint8_t f_hold = 0;
+uint8_t f_hold = OFF;
+
+uint8_t f_zero = OFF;
 
 uint8_t fEjeY = OFF;
 
@@ -133,6 +143,8 @@ void tarea_display(void *p);
 void tarea_orienta(void *p);
 void tarea_refresh(void *p);
 void tarea_pulsadores(void *p);
+void tarea_modos(void *p);
+
 /* USER CODE END 0 */
 
 
@@ -216,9 +228,10 @@ int main(void)
 	//agregar_tarea(lista_tareas, tarea_iwdg, NULL, 0, 1, 0, 100000);
 	agregar_tarea(lista_tareas, tarea_led_blinking, NULL, 0, 10, 0, 4); // et_wcet = 2
 	agregar_tarea(lista_tareas, tarea_orienta, NULL, 0, 1, 0, 100000);   	// et_wcet = 926
-	agregar_tarea(lista_tareas, tarea_display, NULL, 0, 1, 0, 100000); 	// et_wcet = 7876
     agregar_tarea(lista_tareas, tarea_refresh, NULL, 0, 1, 0, 100000);  	// et_wcet = 3632
     agregar_tarea(lista_tareas, tarea_pulsadores, NULL, 0, 10, 0, 100000);  	// et_wcet =
+    agregar_tarea(lista_tareas, tarea_modos, NULL, 0, 10, 0, 100000);  	// et_wcet =
+    agregar_tarea(lista_tareas, tarea_display, NULL, 0, 1, 0, 100000); 	// et_wcet = 7876
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -266,6 +279,9 @@ uint8_t antirebote (uint8_t teclaActual)
 	return NO_KEY;
 }
 
+
+// TAREAS
+
 void tarea_pulsadores (void *p)
 {
 
@@ -281,97 +297,188 @@ void tarea_pulsadores (void *p)
 		tecla = HOLD;
 	}
 
-	tecla = antirebote(tecla);
+	if (tecla != NO_KEY)
+		tecla = antirebote(tecla);
 
-//	switch(tecla){
-//	case NO_KEY:
-//		// que hago? help me!
-//		break;
-//	case MODE:
-//		// Cambio de modo
-//		mode++;
-//		mode %= CANT_MODOS;
-//		break;
-//	case ZERO:
-//		// OFFSET
-//		break;
-//	case HOLD:
-//		// Congelar el valor
-//		hold = ON;
-//		break;
-//	}
 }
 
 void tarea_modos(void *p)
 {
+	static uint8_t clean = OFF;
 	static uint8_t mode = MODO_MEDIR;
 
 	if(tecla == MODE){
 		mode++;
 		mode%=CANT_MODOS;
+		clean = OFF;
 	}
 
 	switch(mode){
 	case MODO_MEDIR:
+		if(clean == OFF){
+			SSD1306_Fill(SSD1306_COLOR_BLACK); /* Clear screen */
+			offset_x = 0;
+			offset_y = 0;
+			clean = ON;
+		}
 		fEjeY = OFF;
 		if(tecla == HOLD){
 			f_hold = ~ f_hold; // toggle flag
 		}
 		if(tecla == ZERO){
 			// VER COMO IMPLEMENTAR -- NO LO VOY A PENSAR AHORA -- gg ez
+			f_zero = ON;
 		}
 		break;
 
 	case MODO_NIVEL:
+		if(clean == OFF){
+			SSD1306_Fill(SSD1306_COLOR_BLACK); /* Clear screen */
+			offset_x = 0;
+			offset_y = 0;
+			clean = ON;
+		}
 		fEjeY = ON;
 		if(tecla == HOLD){
 			f_hold = ~ f_hold; // toggle flag
 		}
 		if(tecla == ZERO){
 			// VER COMO IMPLEMENTAR -- NO LO VOY A PENSAR AHORA -- gg ez
+			f_zero = ON;
 		}
 		break;
 	case MODO_ALARMAS:
+		if(clean == OFF){
+			SSD1306_Fill(SSD1306_COLOR_BLACK); /* Clear screen */
+			offset_x = 0;
+			offset_y = 0;
+			clean = ON;
+		}
 		fEjeY = OFF;
+		f_hold = OFF;
+
+
 		break;
 	default:
-
+		mode = MODO_MEDIR;
 	}
 }
 
 void tarea_display(void *p)
 {
-	static int cont = 0;
-	static float prom_x = 0;
-	static float prom_y = 0;
-	char str_x[20];
-	char str[20] = {0};
+	static int cont = 0;				//!< Variable contador para el promedio
+	static float prom_x = 0;			//!<
+	static float prom_y = 0;			//!<
+	static float valorAntX = 0;			//!<
+	static float valorAntY = 0;			//!<
+	float prom_offset_x = 0;			//!<
+	float prom_offset_y = 0;			//!<
+	char str_x[5];						//!<
+	char str_y[5];						//!<
+	char str1[6] = {0};				//!<
+	char str2[6] = {0};				//!<
 
 	if(cont >= PROMEDIO){
 		cont = 0;
-		prom_x /= 10;
-		prom_y /= 10;
-		// Chequeo signo
-		if(prom_x < 0){
-			prom_x = -prom_x;
-			str[0] = 118; // v
+		prom_x /= PROMEDIO;
+		prom_y /= PROMEDIO;
+
+		//------------------------------------------------------------------------------------------------
+		//--------------------------------------- Chequeo signo -----------------------------------------
+		//------------------------------------------------------------------------------------------------
+		if(offset_x != 0){
+			prom_x = fabs(prom_x);
+
+			if((prom_offset_x) < 0)
+				str1[0] = 118; // v
+			else
+				str1[0] = 94; // ^
 		}
 		else{
-			str[0] = 94; // ^
+			if(prom_x < 0)
+				str1[0] = 118; // v
+			else
+				str1[0] = 94; // ^
+			prom_x = fabs(prom_x);
 		}
-		// Redondeo
-		if(prom_x >= 10 ){
-			SSD1306_GotoXY (POS_GRADOS);
-			sprintf(str_x,"%.2f", roundf(prom_x * 100)/100);
-			strcat(str, str_x);
-			SSD1306_Puts(str, &Font_16x26, 1);
+
+		if(offset_y != 0){
+			prom_y = fabs(prom_y);
+
+			if((prom_offset_y) < 0)
+				str2[0] = 118; // v
+			else
+				str2[0] = 94; // ^
 		}
 		else{
-			SSD1306_GotoXY (POS_GRADOS);
-			sprintf(str_x,"0%.2f", roundf(prom_x * 100)/100);
-			strcat(str, str_x);
-			SSD1306_Puts(str, &Font_16x26, 1);
+			if(prom_y < 0)
+				str2[0] = 118; // v
+			else
+				str2[0] = 94; // ^
+			prom_x = fabs(prom_x);
 		}
+
+		prom_offset_x = prom_x - offset_x;
+		prom_offset_y = prom_y - offset_y;
+
+		//------------------------------------------------------------------------------------------------
+		//----------------------------------------------- Zero -------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		if(f_zero == ON){
+			offset_x = prom_x;
+			offset_y = prom_y;
+			f_zero = OFF;
+		}
+
+		//------------------------------------------------------------------------------------------------
+		//------------------------------------------- THRESHOLD ------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		if(fabs(prom_offset_x - valorAntX) < THRESHOLD )
+			prom_offset_x = valorAntX;
+		if(fabs(prom_offset_x - valorAntY) < THRESHOLD )
+			prom_offset_y = valorAntY;
+
+		//------------------------------------------------------------------------------------------------
+		//-------------------------------------------- Valor Anterior ------------------------------------
+		//------------------------------------------------------------------------------------------------
+		valorAntX = prom_offset_x;
+		valorAntY = prom_offset_y;
+
+		//------------------------------------------------------------------------------------------------
+		//---------------------------------------- Redondeo ----------------------------------------------
+		//------------------------------------------------------------------------------------------------
+		if(fabs(prom_offset_x) >= 10 ){
+			sprintf(str_x,"%.2f", roundf(fabs(prom_offset_x) * 100)/100);
+			strcat(str1, str_x);
+		}
+		else{
+			sprintf(str_x,"0%.2f", roundf(fabs(prom_offset_x) * 100)/100);
+			strcat(str1, str_x);
+		}
+		if(prom_offset_y >= 10 ){
+			sprintf(str_y,"%.2f", roundf(fabs(prom_offset_y) * 100)/100);
+			strcat(str2, str_y);
+		}
+		else{
+			sprintf(str_y,"0%.2f", roundf(fabs(prom_offset_y) * 100)/100);
+			strcat(str2, str_y);
+		}
+
+
+		//------------------------------------------------------------------------------------------------
+		//-------------------------------------- MODO 1 o MODO 2 -----------------------------------------
+		//------------------------------------------------------------------------------------------------
+		if(fEjeY == OFF){	// Solo eje X
+			SSD1306_GotoXY (POS_GRADOS);
+			SSD1306_Puts(str1, &Font_16x26, 1);
+		}
+		else{	// Solo eje Y
+			SSD1306_GotoXY(POS_GRADOSX);
+			SSD1306_Puts(str1, &Font_11x18, 1);
+			SSD1306_GotoXY(POS_GRADOSY);
+			SSD1306_Puts(str2, &Font_11x18, 1);
+		}
+
 		prom_x = 0;
 		prom_y = 0;
 	}
@@ -404,7 +511,8 @@ void tarea_led_blinking(void *p)
 
 void tarea_refresh(void *p)
 {
-	SSD1306_UpdateScreen();
+	if(f_hold == OFF)
+		SSD1306_UpdateScreen();
 }
 
 void falla_sistema(void)
@@ -412,8 +520,7 @@ void falla_sistema(void)
 	__disable_irq();
 	while (1)
 	{
-		for (uint32_t i = 0; i < 100000; i++)
-			;
+		for (uint32_t i = 0; i < 100000; i++);
 		HAL_GPIO_TogglePin(GPIOC, Led_Blink_Pin);
 	}
 }
@@ -594,7 +701,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pins : inputHold_Pin inputZero_Pin inputMode_Pin */
   GPIO_InitStruct.Pin = inputHold_Pin|inputZero_Pin|inputMode_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pin : outputLed_Pin */
